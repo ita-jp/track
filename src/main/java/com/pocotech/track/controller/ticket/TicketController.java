65b15e09233ff1b4136255feb708be791f5d99b9
ita-jp <tn00gm@gmail.com>
2022-07-20 16:08:56 +0900

    ticket list

---
+++ src/main/java/com/pocotech/track/controller/ticket/TicketController.java
@@ -0,0 +1,15 @@
+package com.pocotech.track.controller.ticket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/tickets")
+@Controller
+public class TicketController {
+
+    @GetMapping
+    public String list() {
+        return "tickets/list";
+    }
+}
